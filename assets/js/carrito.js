aca8604afad853fe2093f6f7d142b0cfb672fddb
Antonella <ant.orellana@duocuc.cl>
2024-07-01 16:26:47 -0400

    carrito ok

--- assets/js/carrito.js
+++ assets/js/carrito.js
@@ -1,5 +1,3 @@
-// carrito.js
-
 document.addEventListener('DOMContentLoaded', () => {
     actualizarCarritoUI();
 });
@@ -7,64 +5,49 @@ document.addEventListener('DOMContentLoaded', () => {
 function agregarAlCarrito(nombre, precio) {
     const producto = { nombre, precio };
 
-    // Obtener el carrito del localStorage
-    let carrito = obtenerCarritoDesdeLocalStorage();
+    let carrito = carritoLS();
 
-    // Agregar el producto al carrito
     carrito.push(producto);
 
-    // Actualizar el localStorage
     localStorage.setItem('carrito', JSON.stringify(carrito));
 
-    // Actualizar la interfaz de usuario del carrito
     actualizarCarritoUI();
 }
 
 function eliminarDelCarrito(index) {
-    // Obtener el carrito del localStorage
-    let carrito = obtenerCarritoDesdeLocalStorage();
+    let carrito = carritoLS();
 
-    // Eliminar el producto del carrito en la posición `index`
     carrito.splice(index, 1);
 
-    // Actualizar el localStorage
     localStorage.setItem('carrito', JSON.stringify(carrito));
 
-    // Actualizar la interfaz de usuario del carrito
     actualizarCarritoUI();
 }
 
 function vaciarCarrito() {
-    // Limpiar el carrito (eliminar todos los productos)
     localStorage.removeItem('carrito');
 
-    // Actualizar la interfaz de usuario del carrito
     actualizarCarritoUI();
 }
 
 function comprar() {
-    // Limpiar el carrito (eliminar todos los productos)
     localStorage.removeItem('carrito');
 
-    // Redirigir al usuario de vuelta al catálogo
     window.location.href = '/catalogo.html';
 }
 
 function actualizarCarritoUI() {
-    const carrito = obtenerCarritoDesdeLocalStorage();
+    const carrito = carritoLS();
 
     const carritoUI = document.getElementById('carrito');
 
-    // Limpiamos el contenido previo del carrito
     carritoUI.innerHTML = '';
 
-    // Iteramos sobre los productos en el carrito y los mostramos
     carrito.forEach((producto, index) => {
         const li = document.createElement('li');
         li.classList.add('list-group-item');
         li.textContent = `${producto.nombre} - ${producto.precio} CLP`;
 
-        // Botón para eliminar producto del carrito
         const btnEliminar = document.createElement('button');
         btnEliminar.classList.add('btn', 'btn-danger', 'btn-sm', 'ms-2');
         btnEliminar.textContent = 'Eliminar';
@@ -77,6 +60,6 @@ function actualizarCarritoUI() {
     });
 }
 
-function obtenerCarritoDesdeLocalStorage() {
+function carritoLS() {
     return JSON.parse(localStorage.getItem('carrito')) || [];
 }
