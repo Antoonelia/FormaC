5c0dbee7ac1258959898c4e2ba9edf8a3b03de07
Antonella <ant.orellana@duocuc.cl>
2024-07-02 03:51:01 -0400

    proyecto en djangoo

--- usuariosapp/views.py
+++ usuariosapp/views.py
@@ -15,12 +15,7 @@ def catalogo(request):
 def agregar_al_carrito(request, producto_id):
     producto = Producto.objects.get(pk=producto_id)
 
-    # Lógica para agregar al carrito
-    # Puedes usar sesiones o crear un modelo para el carrito del usuario
-
     return redirect('catalogo')
 
 def ver_carrito(request):
-    # Lógica para mostrar el carrito del usuario
-    # Por ejemplo, obtener productos agregados al carrito
     return render(request, 'carrito.html')
